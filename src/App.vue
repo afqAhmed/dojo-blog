856d1f91886a3c1da7efefffd250cb65cba93a64
Ashfaq Ahmed <ahmed.ashfaq9281@gmail.com>
2021-11-24 18:36:06 +0600

    Project Initialized

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Nav />
+  <router-view />
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: { Nav }
+
+}
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-900 text-gray-200 py-5">
+    <div class="container flex place-content-between text-lg uppercase">
+      <div class="">Brand</div>
+      <div class="">
+        <router-link class="px-5 py-3 hover:bg-gray-700 transition duration-300" to="/">Home</router-link>
+        <router-link class="px-5 py-3 hover:bg-gray-700 transition duration-300"  :to="{ name: 'About' }">About</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
