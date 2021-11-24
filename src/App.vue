02883d60acfda8104dd71e87cedfeb4c432b9f52
Ashfaq Ahmed <ahmed.ashfaq9281@gmail.com>
2021-11-24 23:19:55 +0600

    Refs vs Reactive Explained

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,15 @@
 <template>
-    <Nav />
+  <Nav />
   <div class="container">
-    <p ref="para">My name is {{ name }} and I'm {{ age }} years old.</p>
-    <button class="block" @click="handleClick">Click Me</button>
-    <input type="text" v-model="name">
-    <button @click="age++">Add 1 to age</button>
     <router-view />
   </div>
 </template>
 
 <script>
 import Nav from './components/Nav.vue'
-import { ref } from 'vue'
 
 export default {
   name: 'App',
-  components: { Nav },
-  setup() {
-    // let name = 'Ashfaq'
-    // let age = 47
-    const name = ref('Ashfaq')
-    const age = ref(47)
-    
-    const para = ref(null)
-
-    const handleClick = () => {
-      // console.log(para) // value: p
-      // console.log(para.value) // <p>My name is Ashfaq and I'm 47 years old.<p>
-      // para.value.classList.add('active')
-      // para.value.textContent = 'Oops! you change the content'
-      name.value = 'Ashfaq Ahmed'
-      age.value = 46
-    }
-
-    return { name, age, para, handleClick }
-  },
+  components: { Nav }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>Home Page</div>
+  <div>
+    <h1>Home Page</h1>
+    <h2 class="text-blue-500">Refs Example</h2>
+    <p>{{ ninjaOne.name }} -- {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne" class="block bg-gray-400 mt-4 p-2 rounded-lg">Click Me</button>
+    <h2 class="text-blue-500">Reactive Example</h2>
+    <p>{{ninjaTwo.name}} -> {{ninjaTwo.age}}</p>
+    <button @click="updateNinjaTwo" class="block bg-gray-400 mt-4 p-2 rounded-lg">Click Me</button>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Home'
+import { reactive, ref } from 'vue'
 
+export default {
+  name: 'Home',
+  setup() {
+    const ninjaOne = ref({
+      name: 'Ashfaq',
+      age: 47,
+    })
+    const ninjaTwo = reactive({
+      name: 'Susan',
+      age: 37,
+    })
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 46
+    }
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 38
+    }
+
+    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo  }
+  },
 }
 </script>
