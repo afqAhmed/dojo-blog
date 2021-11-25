83712e7c7f497d22594a892e1c7539de6812c08c
Ashfaq Ahmed <ahmed.ashfaq9281@gmail.com>
2021-11-25 10:57:30 +0600

    Computed Property in Composition API Explained

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>About Page</div>
+  <div>
+    <h1>About Page</h1>
+  </div>
+  <input v-model="searchBy" class="border mb-8" type="text">
+  <p class="mb-8">Search by - {{ searchBy }}</p>
+  <div v-for="name in searchedNames" :key="name">{{ name }}</div>
 </template>
 
 <script>
+import { ref, computed } from 'vue'
+
 export default {
-  name: 'About'
+  name: 'About',
+  setup() {
+    const names = ref(['Ashfaq', 'Susan', 'Tahia', 'Sanaf', 'Afsheen', 'Mostaquim', 'Ehsan', 'Kawsar'])
+    const searchBy = ref('')
+    const searchedNames = computed(() => {
+      return names.value.filter(name => name.includes(searchBy.value))
+    })
+
+
+    return { names, searchBy, searchedNames }
+  }
 
 }
 </script>
